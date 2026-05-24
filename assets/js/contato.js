a3aa1e751a5cdbf96d7865ed78441852d6c52e79
Tatiane Amorim <tatiane.marketing@gmail.com>
2026-05-24 15:05:21 -0300

    CORREÇÃO FORM

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -43,9 +43,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const url =
 `https://api.callmebot.com/whatsapp.php?phone=5522997994770&text=${encodeURIComponent(texto)}&apikey=1395824`;
 
-   /*// ABRE WHATSAPP
-    window.location.href = url;*/
-    window.open(url, "_blank");
+   // ABRE WHATSAPP
+    window.location.href = url;
 
     //ENVIA SEM ABRIR PÁGINA
     fetch(url)
