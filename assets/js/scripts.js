b9790482fdb5d872d2f37393ef5948cbcaf5f7b4
Tatiane Amorim <tatiane.marketing@gmail.com>
2026-06-24 14:31:34 -0300

    correção -index/card destaque

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -119,7 +119,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const swiper = new Swiper('.cards-container.swiper', {
   loop: true,
   slidesPerView: 1,
-  spaceBetween: 20,
+  spaceBetween: 40,
 
   // CONFIGURAÇÃO DO MOVIMENTO (CORRIGIDA)
   autoplay: {
@@ -145,7 +145,7 @@ const swiper = new Swiper('.cards-container.swiper', {
 
   // Breakpoints para manter a responsividade que configuramos
   breakpoints: {
-    576: { slidesPerView: 1, spaceBetween: 20 },
+    576: { slidesPerView: 1, spaceBetween: 40 },
     768: { slidesPerView: 3, spaceBetween: 25 },
     1200: { slidesPerView: 4, spaceBetween: 30 }
   }
