427991f3a670a9508d052e04dc2ec03618917ede
Tatiane Amorim <tatiane.marketing@gmail.com>
2026-06-29 16:30:35 -0300

    correçõe finais/index/cards destaque

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,66 +1,32 @@
-
-
-const swiper = new Swiper('.cards-container.swiper', {
+const swiper = new Swiper('.swiper', {
   loop: true,
-  slidesPerView: 1,
-  spaceBetween: 20,
-  centeredSLides: true,
 
-  // CONFIGURAÇÃO DO MOVIMENTO (CORRIGIDA)
   autoplay: {
-    delay: 3000,             // Tempo parado em cada card (ex: 3 segundos)
-    disableOnInteraction: false, // O movimento NÃO morre se o usuário clicar nas setas
-    
+    delay: 3000, 
+    disableOnInteraction: false, 
   },
+  
+  spaceBetween: 15,
+  slidesPerView: 1,
 
-
-  // CONFIGURAÇÃO DAS SETAS (Verifique se as classes batem exatamente com o HTML)
+  pagination: {
+    el: '.swiper-pagination', 
+    clickable: true,
+  },
+  
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
 
-  // Breakpoints para manter a responsividade que configuramos
-
-    breakpoints: {
-  0: {
-      slidesPerView: 1,
-      spaceBetween: 20
-      },
-      
-  576: {
-      slidesPerView: 1,
-      spaceBetween: 20
-      },
-      
-  768: {
+  breakpoints: {
+    768: {
       slidesPerView: 2,
-      spaceBetween: 25
-      },
-  
-  992: {
-    slidesPerView: 3,
-    spaceBetween: 25
-      },
-  
-  1200: {
-    slidesPerView: 4,
-    spaceBetween: 30
-  }
-}
-  });
-
-
-  //OS
-  AOS.init({
-      duration: 1000,
-      once: true
-  });
-       
-
-  
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 25,
+    },
+  },
+});
